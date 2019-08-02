81ed3371d6385dd00128bc54af7066972d127899
Valkyrie <valkyriedev15@gmail.com>
2019-08-02 22:23:15 +0000

    Updated player physics

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Item item;
+
+    [SerializeField]
+    private static readonly float SPEED = 512.0f;
+
+	[SerializeField] private readonly float DAMPENING = 0.01f;
+
+	private Vector2 speed;
+	
+    void Start()
+    {
+
+    }
+
+    private void Update()
+	{
+		Vector2 acceleration = new Vector2();
+
+		if (Input.GetKey(KeyCode.W)) acceleration.y += SPEED;
+		if (Input.GetKey(KeyCode.S)) acceleration.y -= SPEED;
+		if (Input.GetKey(KeyCode.A)) acceleration.x -= SPEED;
+		if (Input.GetKey(KeyCode.D)) acceleration.x += SPEED;
+
+		this.speed += acceleration * Time.deltaTime;
+		this.transform.position += new Vector3(this.speed.x * Time.deltaTime, this.speed.y * Time.deltaTime, 0.0f);
+
+		this.speed *= Mathf.Pow(DAMPENING, Time.deltaTime);
+		PickupCheck();
+    }
+
+    private void PickupCheck()
+    {
+        
+    }
+}
