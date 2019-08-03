4229e05d729101361d9721c9d2ce098fdc010cf8
Jedd Morgan <jeddrmorgan@gmail.com>
2019-08-03 20:46:04 +0000

    Updated menu

---
+++ Assets/Scripts/Main Menu/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void NewGame()
+    {
+        Debug.Log("Loading new scene");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
