b0fcb822436d7b817bc962d1f04e2e154e8957f8
Valkyrie <valkyriedev15@gmail.com>
2019-08-03 12:23:20 +0000

    Added a UI of sorts

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+	[SerializeField] private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player == null)
+		{
+			player = GameObject.FindGameObjectWithTag("Player");
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Item i = player.GetComponent<PlayerController>().heldItem;
+
+		if (i.iName.Equals(""))
+		{
+			this.GetComponent<UnityEngine.UI.Image>().color = Color.clear;
+		}
+		else
+		{
+			this.GetComponent<UnityEngine.UI.Image>().color = Color.white;
+		}
+
+		this.GetComponent<UnityEngine.UI.Image>().sprite = player.GetComponent<PlayerController>().heldItem.iSprite;
+    }
+}
