109d4f317ddc1daeaab0276460087e3109c39876
Jedd Morgan <jeddrmorgan@gmail.com>
2019-08-04 18:44:15 +0000

    New Level

---
+++ Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public GameObject blockPrefab;
+    public Vector2 position;
+  
+
+    [SerializeField] private PuzzleTrigger[] triggers;
+    [SerializeField] private float COOLDOWN = 1f;
+
+    private float cooldown;
+
+
+    private void Update()
+    {
+        cooldown -= Time.deltaTime;
+        bool isUnlocked = true;
+
+
+        foreach (PuzzleTrigger p in triggers)
+        {
+            if (!p.isUnlocked)
+            {
+                isUnlocked = false;
+                break;
+            }
+
+            if (isUnlocked && cooldown <= 0)
+            {
+                Instantiate(blockPrefab, position, Quaternion.identity);
+                cooldown = COOLDOWN;
+            }
+        }
+    }
+}
--- Assets/Scripts/PuzzleDoor.cs
+++ Assets/Scripts/PuzzleDoor.cs
@@ -7,38 +7,59 @@ public class PuzzleDoor : MonoBehaviour
 	[SerializeField] private PuzzleTrigger[] triggers;
     [SerializeField] private GameObject[] activateOnTrigger;
     [SerializeField] private AudioClip DOOR_UNLOCK;
+    [SerializeField] private bool STAY_UNLOCKED;
     private AudioSource audioSource;
+    private Vector2 closedPos;
+    private bool lastState = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
         this.audioSource = this.GetComponent<AudioSource>();
+        closedPos = transform.position;
+    }
+
+    private void toggleUnlocked(bool isUnlocked)
+    {
+        this.audioSource.PlayOneShot(DOOR_UNLOCK);
+
+        if (isUnlocked)
+        {
+            foreach (GameObject g in activateOnTrigger)
+            {
+                g.SetActive(true);
+            }
+            transform.SetPositionAndRotation(new Vector2(255, 255), Quaternion.identity);
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-		bool isUnlocked = true;
+        bool isUnlocked = true;
+		
 
 		foreach (PuzzleTrigger p in triggers)
 		{
 			if (!p.isUnlocked)
 			{
-				isUnlocked = false;
+                transform.SetPositionAndRotation(closedPos, Quaternion.identity);
+                isUnlocked = false;
 				break;
 			}
 		}
+        if(STAY_UNLOCKED && isUnlocked)
+        {
+            Destroy(gameObject);
+        }
 
-		if (isUnlocked)
-		{
-            this.audioSource.PlayOneShot(DOOR_UNLOCK);
-            foreach (GameObject g in activateOnTrigger)
-            {
-                g.SetActive(true);
-            }
+        if(lastState != isUnlocked)
+        {
+            lastState = isUnlocked;
+            toggleUnlocked(isUnlocked);
+        }
 
-            GameObject.Destroy(this.gameObject);
-		}
     }
 }
